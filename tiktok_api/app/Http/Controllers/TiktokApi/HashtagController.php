6f9b64189b02b27a8b5aa29bfad48e10bd3859a3
H-HauDV <doanhau1407@gmail.com>
2022-07-17 14:45:55 +0700

    Hastag API initial

---
+++ tiktok_api/app/Http/Controllers/TiktokApi/HashtagController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers\TiktokApi;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\TiktokApi\Hashtags;
+
+class HashtagController extends Controller
+{
+    //
+    public function load_hashtag(){
+        $all_hashtag = Hashtags::all();
+        return response()->json([
+            'alert' => 200,
+        ]);
+    }
+}
